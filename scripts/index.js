962a7be1e5c4d3b4f55238f34f4b5e41379304e1
Alexander Kondakov <a.kondakov91@yandex.ru>
2023-11-09 20:44:04 +0300

    #1 feat: создание функции в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardPlacesList = document.querySelector('.places__list');
 
 // @todo: DOM узлы
+function addCard(arrElement) {
+  cardPlacesList.append(arrElement);
+}
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteHandler) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  const cardDeletButton = cardElement.querySelector('.card__delete-button');
+  cardDeletButton.addEventListener('click', deleteHandler);
+  
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const listCard = event.target.closest('.card');
+  listCard.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards() {
+  initialCards.forEach(cardData => {
+    const arrElement = createCard(cardData, deleteCard);
+    addCard(arrElement);
+  });
+}
+
+renderCards();
